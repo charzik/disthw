20a2f903d2cbf0b680fdcc86927f7a76756e2805
charzik <misha-char@mail.ru>
2020-03-05 21:24:58 +0000

    work

--- disthw/urls.py
+++ disthw/urls.py
@@ -1,10 +1,9 @@
-from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='API')
 
 urlpatterns = [
-    path('', schema_view),
+    path('docs', schema_view),
     path('v1/', include('items.urls'))
 ]
